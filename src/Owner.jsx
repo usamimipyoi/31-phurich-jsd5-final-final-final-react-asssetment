87dce26e8c8300085fbaac2ffa50369326d7d82d
Phurich Thawornying <phurich.twy@gmail.com>
2023-09-13 23:13:59 +0700

    edit owner

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -4,7 +4,7 @@ const Owner = () => {
             <h1>Phurich - Group N - 31</h1>
             <img src="asset\my-image-for-some-reason.png" alt="yeah, my pic"/>
             <h4>Short Biography:</h4>
-            <p>"My beautiful Madame. You aren't the main character nor the antagonist. Instead, look up the term "plot device" in the dictionary." - Golconde</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, fugit! Deleniti dolore, iusto reiciendis officia nam velit vitae laudantium repellendus consequatur quia laborum placeat sit eius corrupti minus tenetur voluptas, harum ducimus molestias quisquam distinctio? Modi reprehenderit laborum explicabo aut eaque iure iste, temporibus aspernatur adipisci beatae eveniet ratione ut?</p>
         </div>
     )
 }
